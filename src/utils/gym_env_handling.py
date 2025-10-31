21c712ae5336fe66b885088178bdef09617deb7c
Willian-Girao <williansoaresgirao@gmail.com>
2025-10-31 16:59:33 +0100

    animations dir overriden if it exists (bool, default true)

--- src/utils/gym_env_handling.py
+++ src/utils/gym_env_handling.py
@@ -1,8 +1,15 @@
 import os
+import os
+import shutil
 
-def save_animation(frames:list, modeL_dir:os.path, file_name:str) -> None:
+def save_animation(frames:list, modeL_dir:os.path, file_name:str, overrride_existing:bool=True) -> None:
     import imageio
     
     gif_path = os.path.join(modeL_dir, 'animations')
+
+    # Override existing animations
+    if overrride_existing and os.path.exists(gif_path) and os.path.isdir(gif_path):
+        shutil.rmtree(gif_path)
+
     os.makedirs(gif_path, exist_ok=True)
     imageio.mimsave(os.path.join(gif_path, file_name), frames, fps=30)
