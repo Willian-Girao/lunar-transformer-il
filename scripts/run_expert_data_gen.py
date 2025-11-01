f1668d086c5e326afc41d6a36e439d9533839dcd
Willian-Girao <williansoaresgirao@gmail.com>
2025-11-01 15:12:31 +0100

    some loading fn with option for model location + traning now can eval at epoch end

--- scripts/run_expert_data_gen.py
+++ scripts/run_expert_data_gen.py
@@ -8,7 +8,7 @@ os.chdir(project_root) # change working directory to project root.
 nb_episodes = 1000
 
 # Generates the training data
-# os.system(f'"{sys.executable}" -m src.data.gen_lander_expert_data --max_steps 400 --nb_episodes {nb_episodes}')
+os.system(f'"{sys.executable}" -m src.data.gen_lander_expert_data --max_steps 400 --nb_episodes {nb_episodes}')
 
 # Generates some (non-overlapping) evaluation data
 os.system(f'"{sys.executable}" -m src.data.gen_lander_expert_data --max_steps 400 --nb_episodes 100 --start_env_seed {nb_episodes} --is_eval')
